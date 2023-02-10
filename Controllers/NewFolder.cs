024e4773e52ab1969dac994c3e0a91ea7ebe55de
hitarthi patel <hppatel0405@gmail.com>
2023-02-10 16:04:30 +0530

    Add project files.

---
+++ Controllers/NewFolder.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class NewFolder : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
